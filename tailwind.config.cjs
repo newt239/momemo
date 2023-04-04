4078323a3f65f0973b23cdcee6a00c97164016b4
newt239 <ysti2019.win2020.st@gmail.com>
2023-04-04 16:29:58 +0900

    add: set google font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,16 +4,9 @@ module.exports = {
   darkMode: 'class',
   content: [
     './src/**/*.{html,js,svelte,ts}',
-    require('path').join(require.resolve(
-      '@skeletonlabs/skeleton'),
-      '../**/*.{html,js,svelte,ts}'
-    )
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    // 3. Append the Skeleton plugin to the end of this list
-    ...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()
-  ]
+  plugins: []
 }
